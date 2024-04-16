4e018c7d8fccc75fe6b89a89d9010da0a2474e28
hwangjinseong <imhjs2023@gmail.com>
2024-04-16 13:52:04 +0900

    01 : find

--- src/00. Interoperability with non functional code/find.ts
+++ src/00. Interoperability with non functional code/find.ts
@@ -2,7 +2,7 @@ import { Option, fromNullable } from "fp-ts/lib/Option";
 
 export function find<A>(as: Array<A>, predicate: (a: A) => boolean): Option<A> {
   /** 풀이
-   * return as.find(predicate) === undefined ? none : some(result);
+   * return as.find(predicate) === undefined ? none : some(as.find(predicate));
    */
   return fromNullable(as.find(predicate));
 }
