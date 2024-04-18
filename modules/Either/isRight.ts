4cc39e669c358bd2dc11ba293d701664e1933b04
hwangjinseong <imhjs2023@gmail.com>
2024-04-18 13:44:51 +0900

    module : isRight

---
+++ modules/Either/isRight.ts
@@ -0,0 +1,6 @@
+import { Either, Right } from "./Either";
+
+/**
+ * Either가 `Right`의 인스턴스인 경우 `true`를 반환하고, 그렇지 않은 경우 `false`를 반환합니다.
+ */
+export declare const isRight: <A>(ma: Either<unknown, A>) => ma is Right<A>;
