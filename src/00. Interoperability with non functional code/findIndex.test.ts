fbd60019bcb16b0f55a80d4f8a6ed266ffa74603
hwangjinseong <imhjs2023@gmail.com>
2024-04-18 13:34:04 +0900

    00 : findIndex

--- src/00. Interoperability with non functional code/findIndex.test.ts
+++ src/00. Interoperability with non functional code/findIndex.test.ts
@@ -1,22 +1,21 @@
-import { none, some } from "fp-ts/Option";
+import { getEq, isNone, isSome, none, some } from "fp-ts/lib/Option";
+import * as N from "fp-ts/number";
 import { findIndex } from "./findIndex";
 
 describe("findIndex", () => {
-  it("요소를 찾지 못하면 none을 반환해야 합니다.", () => {
-    const as = [1, 2, 3, 4, 5];
-    const predicate = (a: number) => a > 5;
+  const array: number[] = [1, 2, 3, 5];
 
-    const result = findIndex(as, predicate);
-
-    expect(result).toEqual(none);
-  });
+  const E = getEq(N.Eq);
 
   it("요소를 찾으면 some을 반환해야 합니다.", () => {
-    const as = [1, 2, 3, 4, 5];
-    const predicate = (a: number) => a === 3;
+    const result = findIndex(array, (a) => a === 1);
+    expect(isSome(result)).toBeTruthy();
+    expect(E.equals(result, some(0))).toBeTruthy();
+  });
 
-    const result = findIndex(as, predicate);
-
-    expect(result).toEqual(some(2));
+  it("요소를 찾지 못하면 none을 반환해야 합니다.", () => {
+    const result = findIndex(array, (a) => a === 4);
+    expect(isNone(result)).toBeTruthy();
+    expect(E.equals(result, none)).toBeTruthy();
   });
 });
