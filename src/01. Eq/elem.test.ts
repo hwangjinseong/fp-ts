114e7ea77762349d7a1b0c1e0393d4d47f00f1bf
hwangjinseong <imhjs2023@gmail.com>
2024-05-17 12:21:33 +0900

    01 : elem

--- src/01. Eq/elem.test.ts
+++ src/01. Eq/elem.test.ts
@@ -3,10 +3,10 @@ import { eqNumber } from "./eqNumber";
 
 describe("elem", () => {
   it("두 숫자가 동일한 경우 true를 반환해야 합니다", () => {
-    expect(elem(eqNumber)(1, [1, 2, 3])).toBe(true);
+    expect(elem(eqNumber)(1, [1, 2, 3])).toBeTruthy();
   });
 
   it("두 숫자가 다른 경우 false를 반환해야 합니다", () => {
-    expect(elem(eqNumber)(4, [1, 2, 3])).toBe(false);
+    expect(elem(eqNumber)(4, [1, 2, 3])).toBeFalsy();
   });
 });
