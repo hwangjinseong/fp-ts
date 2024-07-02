66c150395ab97d73310d0974cd861d9dc05e5604
hwangjinseong <imhjs2023@gmail.com>
2024-07-02 19:03:10 +0900

    04 : firstMonoid

---
+++ src/04. Monoid/firstMonoid.ts
@@ -0,0 +1,5 @@
+import { getMonoid } from "fp-ts/lib/Option";
+import { first } from "fp-ts/lib/Semigroup";
+
+// 가장 왼쪽에 있는 값을 반환하는 Monoid 만약 None 이라면 오른쪽 값을 반환합니다.
+export const firstMonoid = getMonoid<number>(first());
